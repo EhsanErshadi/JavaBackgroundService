fe3c442367e3e5cbfd48464bfc2972557d6c4b3a
Ehsan Zargar-Ershadi <ershadi.ehsan@gmail.com>
2018-04-18 13:13:44 +0100

    Remove runnable

--- src/main/java/com/example/jbs/App.java
+++ src/main/java/com/example/jbs/App.java
@@ -1,30 +1,79 @@
 package com.example.jbs;
 
-import com.example.jbs.*;
-/**
- * Hello world!
- *
- */
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
-public class App 
-{
-	private static MainRunnable mainRunnable;
+import com.example.jbs.*;
+
+public class App {
+	// private static MainRunnable mainRunnable;
 	private static Thread mainThread;
-	
-    public static void main( String[] args )
-    {
-    	System.out.println( "Please use start stop as background service." );
-    	start();
-    }
-    
-    public static void start() {
-    	mainRunnable = new MainRunnable();
-    	mainThread = new Thread(mainRunnable);
-    	mainThread.start();
-    }
-    
-    public static void stop() {    	
-    	mainThread.interrupt();
-    	mainThread = null;    	
-    }
+
+	private static String host = "localhost";
+	private static int port = 58939;
+	private static int sendPacketInterval = 1000; // ms
+
+	public static void main(String[] args) {
+		System.out.println("Please use start stop as background service.");
+	}
+
+	public static void start(String[] args) {
+		SendUdp("starting ...");
+		// mainRunnable = new MainRunnable();
+		mainThread = new Thread() {
+			public void run() {
+				SendUdp("Thread Running");
+
+				int i = 0;
+				while (true) {
+
+					i++;
+					SendUdp("Hello " + i + ", int=" + Thread.currentThread().isInterrupted());
+
+					try {
+						Thread.sleep(sendPacketInterval);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						break;
+					}
+					if (Thread.currentThread().isInterrupted()) {
+						SendUdp("interrupted");
+						break;
+					}
+				}
+				SendUdp("loop done");
+			}
+		};
+		mainThread.start();
+		SendUdp("start done");
+	}
+
+	public static void stop(String[] args) throws Exception {
+		SendUdp("stopping ...");
+		mainThread.interrupt();
+		SendUdp("stop done");
+		// System.exit(0);
+	}
+
+	public static void SendUdp(String msg) {
+		try {
+			System.out.println(msg);
+			byte[] message = msg.getBytes();
+
+			// Get the Internet address of the specified host
+			InetAddress address = InetAddress.getByName(host);
+
+			// Initialize a datagram packet with data and address
+			DatagramPacket packet = new DatagramPacket(message, message.length, address, port);
+
+			// Create a datagram socket, send the packet through it, close it.
+			DatagramSocket dsocket = new DatagramSocket();
+			dsocket.send(packet);
+			dsocket.close();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
 }
